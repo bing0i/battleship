a6579389bc8ed3aa10511996015d07775cc14455
Courgette <courgett3@gmail.com>
2021-02-20 22:25:16 +0700

    Create Gameboard factory

--- script.js
+++ script.js
@@ -19,41 +19,56 @@ const Ship = (pos) => {
   };
 };
 
-const Gameboard = () => {
-  const ships = [
-    Ship({
-      start: {
-        x: 1,
-        y: 1,
-      },
-      end: {
-        x: 2,
-        y: 1,
-      },
-      length: 2,
-      hitPosition: [],
-      orientation: 'v',
-    }),
-    Ship({
-      start: {
-        x: 3,
-        y: 2,
-      },
-      end: {
-        x: 6,
-        y: 2,
-      },
-      length: 4,
-      hitPosition: [],
-      orientation: 'v',
-    }),
-  ];
+const Gameboard = (sh) => {
+  const ships = Array.from(sh);
+  const sunkenShips = [];
   const missedAttacks = [];
+  const getDirectionPoints = (shipPos, pos) => {
+    const directionPoints = {
+      shipPoint: -1,
+      posPoint: -1,
+    };
 
-  const placeShips = () => {};
+    if (shipPos.orientation === 'v' && shipPos.start.x === pos.x) {
+      directionPoints.shipPoint = shipPos.start.y;
+      directionPoints.posPoint = pos.y;
+    } else if (shipPos.orientation === 'h' && shipPos.start.y === pos.y) {
+      directionPoints.shipPoint = shipPos.start.x;
+      directionPoints.posPoint = pos.x;
+    }
 
-  const receiveAttack = () => {};
-  return {};
+    return directionPoints;
+  };
+
+  const receiveAttack = (pos) => {
+    for (let i = 0; i < ships.length; i++) {
+      let { shipPoint, posPoint } = getDirectionPoints(ships[i].position, pos);
+      if (shipPoint !== -1 && posPoint !== -1) {
+        for (let j = 0; j < ships[i].position.length; j++) {
+          if (shipPoint + j === posPoint) {
+            ships[i].hit(pos);
+            if (ships[i].isSunk()) {
+              sunkenShips.push(i);
+            }
+            return true;
+          }
+        }
+      }
+    }
+
+    missedAttacks.push(pos);
+    return false;
+  };
+
+  const isAllSunk = () => {
+    return ships.length === sunkenShips.length;
+  };
+
+  return {
+    missedAttacks,
+    receiveAttack,
+    isAllSunk,
+  };
 };
 
 const Player = () => {
--- test.test.js
+++ test.test.js
@@ -52,3 +52,119 @@ test('Ship: Those positions are hit', () => {
   aShip.hit({ x: 3, y: 2 });
   expect(aShip.isSunk()).toBe(true);
 });
+
+test('Gameboard: Make missed attacks', () => {
+  const ships = [
+    Ship({
+      start: {
+        x: 1,
+        y: 1,
+      },
+      end: {
+        x: 2,
+        y: 1,
+      },
+      length: 2,
+      hitPosition: [],
+      orientation: 'v',
+    }),
+    Ship({
+      start: {
+        x: 3,
+        y: 2,
+      },
+      end: {
+        x: 6,
+        y: 2,
+      },
+      length: 4,
+      hitPosition: [],
+      orientation: 'h',
+    }),
+  ];
+
+  const gameboard = Gameboard(ships);
+  gameboard.receiveAttack({ x: 4, y: 3 });
+  gameboard.receiveAttack({ x: 5, y: 1 });
+  expect(gameboard.missedAttacks).toEqual([
+    { x: 4, y: 3 },
+    { x: 5, y: 1 },
+  ]);
+});
+
+test('Gameboard: Make hit attacks', () => {
+  const ships = [
+    Ship({
+      start: {
+        x: 1,
+        y: 1,
+      },
+      end: {
+        x: 2,
+        y: 1,
+      },
+      length: 2,
+      hitPosition: [],
+      orientation: 'v',
+    }),
+    Ship({
+      start: {
+        x: 3,
+        y: 2,
+      },
+      end: {
+        x: 6,
+        y: 2,
+      },
+      length: 4,
+      hitPosition: [],
+      orientation: 'h',
+    }),
+  ];
+
+  const gameboard = Gameboard(ships);
+  gameboard.receiveAttack({ x: 1, y: 1 });
+  gameboard.receiveAttack({ x: 4, y: 2 });
+  expect(gameboard.missedAttacks.length).toBe(0);
+});
+
+test('Gameboard: Make all ships sink', () => {
+  const ships = [
+    Ship({
+      start: {
+        x: 1,
+        y: 6,
+      },
+      end: {
+        x: 1,
+        y: 7,
+      },
+      length: 2,
+      hitPosition: [],
+      orientation: 'v',
+    }),
+    Ship({
+      start: {
+        x: 3,
+        y: 2,
+      },
+      end: {
+        x: 6,
+        y: 2,
+      },
+      length: 4,
+      hitPosition: [],
+      orientation: 'h',
+    }),
+  ];
+
+  const gameboard = Gameboard(ships);
+  gameboard.receiveAttack({ x: 1, y: 6 });
+  gameboard.receiveAttack({ x: 1, y: 7 });
+  gameboard.receiveAttack({ x: 3, y: 2 });
+  gameboard.receiveAttack({ x: 4, y: 2 });
+  gameboard.receiveAttack({ x: 5, y: 2 });
+  gameboard.receiveAttack({ x: 6, y: 2 });
+  expect(gameboard.missedAttacks.length).toBe(0);
+  expect(gameboard.isAllSunk()).toBe(true);
+});
