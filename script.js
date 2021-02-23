28645faa9acf3b8fd59dc12ffb76addb7e1552df
Courgette <courgett3@gmail.com>
2021-02-23 22:39:50 +0700

    Create reponsive squares in grid layout by using padding-bottom trick

--- script.js
+++ script.js
@@ -109,4 +109,28 @@ const Computer = (enemyGameboard) => {
   return { play, movedPosition };
 };
 
+const userInterface = (() => {
+  const createGameboards = () => {
+    const gameboards = Array.from(document.querySelectorAll('.gameboard'));
+    for (let i = 1; i <= 10; i++) {
+      for (let j = 1; j <= 10; j++) {
+        for (let k = 0; k < gameboards.length; k++) {
+          let square = document.createElement('div');
+          square.className = `square position-${j}-${i}`;
+          let content = document.createElement('div');
+          content.className = `content`;
+          square.appendChild(content);
+          gameboards[k].appendChild(square);
+        }
+      }
+    }
+  };
+
+  return {
+    createGameboards,
+  };
+})();
+
+userInterface.createGameboards();
+
 export { Ship, Gameboard, Player, Computer };
